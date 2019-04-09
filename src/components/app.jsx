79df83d5464a8cc37965ec97e2f60a588e8d427b
filipe custodio <filipe.l.custodio@gmail.com>
2019-04-10 00:17:07 +0100

    Created an array of gif objects and imported giflist to app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 import SearchBar from './search_bar';
 import Gif from './gif';
+import GifList from './gif_list';
 
 class App extends Component {
   render() {
+    const gifs = [
+      { id: "3d5O10XObbr8LW4bDY" },
+      { id: "l1KsDfM293DMW4SXu" }
+    ];
     return (
       <div>
         <div className="left-scene">
@@ -13,7 +18,9 @@ class App extends Component {
             <Gif id="3d5O10XObbr8LW4bDY" />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs = {gifs} />
+        </div>
       </div>
     );
   }
