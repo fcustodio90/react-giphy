98d6dd0971b643518102af816e0069800b3f7240
filipe custodio <filipe.l.custodio@gmail.com>
2019-04-10 00:47:00 +0100

    Implemented giphy API for dynamic respondes on change event at searchbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "3d5O10XObbr8LW4bDY"
+    };
+  }
+
+  search = (query) => {
+    // Implement the API call
+    giphy('QwYuxMpCToBNTjATlSPDGcv5Ux28hkKt').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
   render() {
-    const gifs = [
-      { id: "3d5O10XObbr8LW4bDY" },
-      { id: "l1KsDfM293DMW4SXu" }
-    ];
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id="3d5O10XObbr8LW4bDY" />
+            <Gif id= {this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs = {gifs} />
+          <GifList gifs = {this.state.gifs} />
         </div>
       </div>
     );
