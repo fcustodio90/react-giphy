78e3c2af010c8fceafd4402f60cc91fa4d531dc8
filipe custodio <filipe.l.custodio@gmail.com>
2019-04-09 21:06:35 +0100

    Removed test code and imported App component to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,44 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
-
-// const Hello = (props) => {
-//   return <div>Hello, {props.name}</div>;
-// };
-
-class Hello extends Component {
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      clicked: false,
-      counter: 0
-    };
-  }
-
-  handleClick = () => {
-    // TODO: Change the state!
-    this.setState({
-      clicked: !this.state.clicked,
-      counter: this.state.counter + 1
-    });
-  }
-
-  render () {
-    // BUILD AND RETURN HTML
-    return (
-      <div className={this.state.clicked ? 'clicked' : null} onClick = {this.handleClick}>
-        Hello {this.props.name} {this.state.counter}
-      </div>
-    );
-  }
-}
+import App from './components/app.jsx';
 
 const root = document.getElementById('root');
 
 if (root) {
   ReactDOM.render(
-    <Hello name="Filipe Custódio" />,
+    <App />,
     root
   );
 }
